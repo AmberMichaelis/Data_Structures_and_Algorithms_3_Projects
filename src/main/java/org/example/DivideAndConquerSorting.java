22172d235859590699491ea8eb23bb281f2fce66
Amber M <amberswieczkowski@pm.me>
2023-01-29 20:33:16 -0600

    First attempt at lsdRadixSort on DivideandConquerSorting.

---
+++ src/main/java/org/example/DivideAndConquerSorting.java
@@ -0,0 +1,144 @@
+package org.example;
+import java.util.Comparator;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Your implementation of various divide & conquer sorting algorithms.
+ */
+
+
+public class DivideAndConquerSorting {
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(n log n)
+     * And a best case running time of: O(n log n)
+     *
+     * You can create more arrays to run merge sort, but at the end, everything
+     * should be merged back into the original T[] which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * Hint: You may need to create a helper method that merges two arrays
+     * back into the original T[] array. If two data are equal when merging,
+     * think about which subarray you should pull from first.
+     *
+     * You may assume that the passed in array and comparator are both valid
+     * and will not be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array to be sorted.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        if (arr.length < 2) {
+            return;
+        }
+        int length = arr.length;
+        int midIndex = length / 2;
+        T[] left = (T[]) new Object[midIndex];
+        T[] right = (T[]) new Object[length - left.length];
+        for (int i = 0; i < midIndex; i++) {
+            left[i] = arr[i];
+        }
+        for (int j = midIndex; j < length; j++) {
+            right[j - midIndex] = arr[j];
+        }
+        mergeSort(left, comparator);
+        mergeSort(right, comparator);
+        merge(left, right, arr, comparator);
+    }
+
+    private static <T> void merge(T[] left, T[] right, T[] result, Comparator<T> comparator) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+            if (comparator.compare(left[i], right[j]) < 0) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+    }
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * It should be:
+     * out-of-place
+     * stable
+     * not adaptive
+     *
+     * Have a worst case running time of: O(kn)
+     * And a best case running time of: O(kn)
+     *
+     * Feel free to make an initial O(n) passthrough of the array to
+     * determine k, the number of iterations you need.
+     *
+     * At no point should you find yourself needing a way to exponentiate a
+     * number; any such method would be non-O(1). Think about how you can
+     * get each power of BASE naturally and efficiently as the algorithm
+     * progresses through each digit.
+     *
+     * You may use an ArrayList or LinkedList if you wish, but it should only
+     * be used inside radix sort and any radix sort helpers. Do NOT use these
+     * classes with merge sort. However, be sure the List implementation you
+     * choose allows for stability while being as efficient as possible.
+     *
+     * Do NOT use anything from the Math class except Math.abs().
+     *
+     * You may assume that the passed in array is valid and will not be null.
+     *
+     * @param arr The array to be sorted.
+     */
+    public static void lsdRadixSort(int[] arr) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        LinkedList<Integer>[] buckets = new LinkedList[19];
+        for (int i = 0; i < 19; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        int base = 10;
+        int div = 1;
+        boolean cont = true;
+        while (cont) {
+            cont = false;
+            for (int num : arr) {
+                int bucket = num / div;
+                if (bucket / 10 != 0) {
+                    cont = true;
+                }
+                if (buckets[bucket % base + 9] == null) {
+                    buckets[bucket % base + 9] = new LinkedList<>();
+                }
+                buckets[bucket % base + 9].add(num);
+            }
+            int arrIdx = 0;
+            for (LinkedList<Integer> bucket : buckets) {
+                if (bucket != null) {
+                    for (int num : bucket) {
+                        arr[arrIdx++] = num;
+                    }
+                    bucket.clear();
+                }
+            }
+            div *= 10;
+        }
+    }
+}
+
